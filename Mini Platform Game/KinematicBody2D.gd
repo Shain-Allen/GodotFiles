a53791ac2a6875a28d003a1a8c6ac91d9869be33
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-04-21 18:31:14 -0700

    finished the turial stuff
    heck, this is easy

--- Mini Platform Game/KinematicBody2D.gd
+++ Mini Platform Game/KinematicBody2D.gd
@@ -1,22 +1,41 @@
+#player movement.gd
 extends KinematicBody2D
 
+const UP = Vector2(0, -1)
 var motion = Vector2()
+export var ACCELERATION = 50
+export var Max_Speed = 200
+export var Jump_Force = 550
+export var Gravity = 10
 
 func _physics_process(delta):
-	motion.y += 10
+	motion.y += Gravity
+	var friction = false
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
-	
+		motion.x = min(motion.x + ACCELERATION, Max_Speed)
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
-	
+		motion.x = max(motion.x - ACCELERATION, -Max_Speed)
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
-		motion.x = 0
-	
+		$Sprite.play("Idle")
+		friction = true
+		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			motion.y = -400
-	
-	move_and_slide(motion)
-	
+			motion.y = -Jump_Force
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
+	else:
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
+			
+	motion = move_and_slide(motion, UP)
+	pass
