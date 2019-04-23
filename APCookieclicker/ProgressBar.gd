fdb40147d19214887555720dd45f376fccb4b6d5
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-04-22 17:36:52 -0700

    heck

--- APCookieclicker/ProgressBar.gd
+++ APCookieclicker/ProgressBar.gd
@@ -4,5 +4,4 @@ extends ProgressBar
 
 
 func _process(delta):
-	while get_node("Timer").RemainingTime < 0:
-		set_percent_visible(get_node("Timer").RemainingTime)
+	set_percent_visible(get_node_or_null(("Timer").RemainingTime))
