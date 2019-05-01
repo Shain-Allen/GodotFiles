4bf9f43da0cf3f6642fdb08421829779080797ec
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-05-01 06:52:23 -0700

    start the upgradeing

--- APCookieclicker/Code/Global.gd
+++ APCookieclicker/Code/Global.gd
@@ -5,4 +5,8 @@ extends Node
 var Total_money = 100.00
 enum Metals {Copper, Bronze, Silver, Gold, Platinum}
 var Metal_amount = [0, 0, 0, 0, 0]
+var MetalProductionTime = [0, 0, 0, 0, 0]
+var Metal_cost = [0, 0, 0, 0, 0]
 var Coin_amount = [0, 0, 0, 0, 0,]
+var CoinProductionTime = [0, 0, 0, 0, 0]
+var Coin_Price = [0, 0, 0, 0, 0]
