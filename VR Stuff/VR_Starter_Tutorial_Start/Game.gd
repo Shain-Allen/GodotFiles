835f543ce7413f98e65de5d38732298ad11ef917
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-09-25 19:26:35 -0700

    broken

--- VR Stuff/VR_Starter_Tutorial_Start/Game.gd
+++ VR Stuff/VR_Starter_Tutorial_Start/Game.gd
@@ -1,12 +1,9 @@
 extends Spatial
 
 func _ready():
-	var VR = ARVRServer.find_interface("OpenVR")
-	if VR and VR.initialize():
+	var arvr_interface = ARVRServer.find_interface("OpenVR")
+	if(arvr_interface and arvr_interface.initialize()):
 		get_viewport().arvr = true
 		get_viewport().hdr = false
-		
-		OS.vsync_enabled = false
-		Engine.target_fps = 90
 		# Also, the physics FPS in the project settings is also 90 FPS. This makes the physics
         # run at the same frame rate as the display, which makes things look smoother in VR!
